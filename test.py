44e3ae1de5ad19bd00a2e38122831162a86abb11
user <tigrtolstoy@yandex.ru>
2018-09-24 14:03:48 +0300

    First

---
+++ test.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np 
+
+
+camera_params = np.load("ik_cam(640x480).npz")
+
+newcameramtx = camera_params["newcameramtx"]
+roi = camera_params["roi"]
+mtx = camera_params["mtx"]
+dist = camera_params["dist"]
+
+stream = cv2.VideoCapture(0)
+
+RUN = True
+
+if __name__ == "__main__":
+
+    while RUN:
+        ret, img = stream.read()
+        undistort = cv2.undistort(img, mtx, dist, None, newcameramtx)
+        x,y,w,h = roi
+        undistort2 = undistort[y:y+h, x:x+w]
+        if ret:
+            cv2.imshow('undistort', undistort)
+            cv2.imshow('undistort2', undistort2)
+            cv2.imshow('original', img)
+            if cv2.waitKey(10) & 0xFF == 27:
+                RUN = not RUN
+        else:
+            print("Can't read data from camera!")
+            RUN = not RUN
